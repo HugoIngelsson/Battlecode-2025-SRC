244a2e284ce2a4cdab977b78311f251c3c404ad5
HugoIngelsson <cingelsson@ucsd.edu>
2025-01-08 16:19:44 -0800

    Added better tower logic

--- robot4/MoneyTower.java
+++ robot4/MoneyTower.java
@@ -1,19 +1,28 @@
-package robot3;
+package robot4;
 
 import battlecode.common.GameActionException;
-import battlecode.common.GameConstants;
+import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
 import battlecode.common.UnitType;
 
-public class PaintTower extends Tower {
+public class MoneyTower extends Tower {
     int nextSpawn = 1;
-    public PaintTower(RobotController rc) throws GameActionException {
+    public MoneyTower(RobotController rc) throws GameActionException {
         super(rc);
+        this.AOE_DMG = 10;
+        this.TARGET_DMG = 20;
+        this.ATTACK_RANGE_SQ = 9;
     }
 
     void play() throws GameActionException {
+        rc.attack(null);
+        MapLocation target = bestAttackTarget();
+        if (target != null) {
+            rc.attack(target);
+        }
+
         if (nextSpawn == 0 && rc.getPaint() >= 100 &&
-                (rc.getMoney() >= 1030 || rc.getRoundNum() < 5 && rc.getMoney() >= 300)) {
+                (rc.getMoney() >= 1030 || rc.getRoundNum() == 1 && rc.getMoney() >= 300)) {
             for (int i=11; i>=0; i--) {
                 if (rc.canBuildRobot(UnitType.MOPPER, spawnPlaces[i])) {
                     rc.buildRobot(UnitType.MOPPER, spawnPlaces[i]);
@@ -23,7 +32,7 @@ public class PaintTower extends Tower {
             nextSpawn++;
         }
         if (nextSpawn == 1 && rc.getPaint() >= 200 &&
-                (rc.getMoney() >= 1030 || rc.getRoundNum() < 5 && rc.getMoney() >= 250)) {
+                (rc.getMoney() >= 1030 || rc.getRoundNum() == 1 && rc.getMoney() >= 250)) {
             for (int i=11; i>=0; i--) {
                 if (rc.canBuildRobot(UnitType.SOLDIER, spawnPlaces[i])) {
                     rc.buildRobot(UnitType.SOLDIER, spawnPlaces[i]);
--- robot4/PaintTower.java
+++ robot4/PaintTower.java
@@ -1,7 +1,7 @@
-package robot3;
+package robot4;
 
 import battlecode.common.GameActionException;
-import battlecode.common.GameConstants;
+import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
 import battlecode.common.UnitType;
 
@@ -9,10 +9,25 @@ public class PaintTower extends Tower {
     int nextSpawn = 1;
     public PaintTower(RobotController rc) throws GameActionException {
         super(rc);
+        this.AOE_DMG = 10;
+        this.TARGET_DMG = 20;
+        this.ATTACK_RANGE_SQ = 9;
     }
 
     void play() throws GameActionException {
-        if (nextSpawn == 0 && rc.getPaint() >= 100 &&
+        rc.attack(null);
+        MapLocation target = bestAttackTarget();
+        if (target != null) {
+            rc.attack(target);
+        }
+
+        if (rc.getType() == UnitType.LEVEL_ONE_PAINT_TOWER && rc.getMoney() >= 5000) {
+            rc.upgradeTower(rc.getLocation());
+        }
+        else if (rc.getType() == UnitType.LEVEL_TWO_PAINT_TOWER && rc.getMoney() >= 7500) {
+            rc.upgradeTower(rc.getLocation());
+        }
+        else if (nextSpawn == 0 && rc.getPaint() >= 100 &&
                 (rc.getMoney() >= 1030 || rc.getRoundNum() < 5 && rc.getMoney() >= 300)) {
             for (int i=11; i>=0; i--) {
                 if (rc.canBuildRobot(UnitType.MOPPER, spawnPlaces[i])) {
@@ -22,7 +37,7 @@ public class PaintTower extends Tower {
             }
             nextSpawn++;
         }
-        if (nextSpawn == 1 && rc.getPaint() >= 200 &&
+        else if (nextSpawn == 1 && rc.getPaint() >= 200 &&
                 (rc.getMoney() >= 1030 || rc.getRoundNum() < 5 && rc.getMoney() >= 250)) {
             for (int i=11; i>=0; i--) {
                 if (rc.canBuildRobot(UnitType.SOLDIER, spawnPlaces[i])) {
